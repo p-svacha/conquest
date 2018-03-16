b82c4a1c611777a6c97ae185c72e8e930424cec4
Philip Svacha <philip.svacha@gmail.com>
2018-03-16 10:42:10 +0100

    UI improvements

--- Conquest/UI/UIManager.cs
+++ Conquest/UI/UIManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Conquest.UI
@@ -11,9 +12,9 @@ namespace Conquest.UI
     class UIManager
     {
         private Label CoordinatesLabel;
-        private StackPanel InfoPanel;
+        private Grid InfoPanel;
 
-        public UIManager(StackPanel infoPanel, Label coordinatesLabel)
+        public UIManager(Grid infoPanel, Label coordinatesLabel)
         {
             CoordinatesLabel = coordinatesLabel;
             InfoPanel = infoPanel;
@@ -21,17 +22,26 @@ namespace Conquest.UI
 
         public void SetCountryInfo(Country c)
         {
-            InfoPanel.Children.Clear();
+            SetupCountryInfoGrid();
+            if (c.Id != -1) {
+                Label valTitle = InfoLabel(c.Id + "", 0, 0, 2);
+                valTitle.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+                valTitle.FontWeight = FontWeights.Bold;
 
-            Label title = new Label();
-            title.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
-            string s = c.Id + " (";
-            foreach (Country n in c.Neighbours) s += n.Id + ", ";
-            s = s.Substring(0, s.Length - 2);
-            s += ")";
-            if (c.Id == -1) title.Content = "no man's land";
-            else title.Content = s;
-            InfoPanel.Children.Add(title);
+                //neighbours
+                Label lblNeighbours = InfoLabel("Neighbours", 1, 0);
+                string neighbourContent = "";
+                if (c.Neighbours.Count > 0)
+                {
+                    foreach (Country n in c.Neighbours) neighbourContent += n.Id + ", ";
+                    neighbourContent = neighbourContent.Substring(0, neighbourContent.Length - 2);
+                }
+                Label valNeighbours = InfoLabel(neighbourContent, 1, 1);
+
+                //army
+                Label lblArmy = InfoLabel("Army", 2, 0);
+                Label valArmy = InfoLabel(c.Army + "", 2, 1);
+            }
         }
 
         public void SetCoordinates(int x, int y)
@@ -39,5 +49,38 @@ namespace Conquest.UI
             CoordinatesLabel.Content = x + " / " + y;
         }
 
+        private Label InfoLabel(string content, int row, int column, int columnSpan = 1)
+        {
+            Label l = new Label()
+            {
+                Content = content
+            };
+            l.SetValue(Grid.RowProperty, row);
+            l.SetValue(Grid.ColumnProperty, column);
+            l.SetValue(Grid.ColumnSpanProperty, columnSpan);
+
+            InfoPanel.Children.Add(l);
+            return l;
+        }
+
+        private void SetupCountryInfoGrid()
+        {
+            InfoPanel.Children.Clear();
+            InfoPanel.ColumnDefinitions.Clear();
+            InfoPanel.RowDefinitions.Clear();
+            InfoPanel.ColumnDefinitions.Add(new ColumnDefinition()
+            {
+                Width = new GridLength(1, GridUnitType.Star)
+            });
+            InfoPanel.ColumnDefinitions.Add(new ColumnDefinition()
+            {
+                Width = new GridLength(1, GridUnitType.Star)
+            });
+            InfoPanel.RowDefinitions.Add(new RowDefinition());
+            InfoPanel.RowDefinitions.Add(new RowDefinition());
+            InfoPanel.RowDefinitions.Add(new RowDefinition());
+        }
     }
+
+
 }

0f97ee97a35784668cb6945a9a0eb2cfe981b93c
Philip Svacha <philip.svacha@gmail.com>
2018-06-14 16:22:23 +0200

    start racesim

---
+++ RaceSimulator/Driver.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace RaceSimulator
+{
+    class Driver
+    {
+        public string Name { get; }
+        public string Country { get; }
+        public int Rating { get; set; }
+        public int SeasonPoints { get; set; }
+
+        public int RaceTime { get; set; }
+
+        public Driver(string Name, string Country, int Rating)
+        {
+            this.Name = Name;
+            this.Country = Country;
+            this.Rating = Rating;
+        }
+
+        public Grid DriverRatingPanel()
+        {
+            Grid grid = new Grid { Height = 40 };
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new System.Windows.GridLength(50, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new System.Windows.GridLength(180, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new System.Windows.GridLength(70, GridUnitType.Pixel) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new System.Windows.GridLength(100, GridUnitType.Pixel) });
+
+            Label rankLabel = new Label { Name = "rankLabel", VerticalAlignment = VerticalAlignment.Center };
+            rankLabel.SetValue(Grid.ColumnProperty, 0);
+            Label nameLabel = new Label { Content = Name, VerticalAlignment = VerticalAlignment.Center };
+            nameLabel.SetValue(Grid.ColumnProperty, 1);
+            Image countryImage = new Image { Source = new BitmapImage(new Uri("C:\\Microsoft\\conquest\\RaceSimulator\\res\\countries\\" + Country + ".png")), Height = 35, VerticalAlignment = VerticalAlignment.Center };
+            countryImage.SetValue(Grid.ColumnProperty, 2);
+            Label ratingLabel = new Label { Content = Rating, Name = "ratingLabel", VerticalAlignment = VerticalAlignment.Center };
+            ratingLabel.SetValue(Grid.ColumnProperty, 3);
+
+            grid.Children.Add(rankLabel);
+            grid.Children.Add(nameLabel);
+            grid.Children.Add(countryImage);
+            grid.Children.Add(ratingLabel);
+
+            return grid;
+        }
+    }
+}
---
+++ RaceSimulator/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace RaceSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Driver> Drivers = new List<Driver>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            Excel.Application xlApp = null;
+            Excel._Worksheet driverRatingsWorksheet = null;
+            Excel._Worksheet currentSeasonWorksheet = null;
+            Excel.Range driverRatingsRange = null;
+            Excel.Range currentSeasonRange = null;
+            Excel.Workbook xlWorkbook = null;
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWorkbook = xlApp.Workbooks.Open("C:\\Microsoft\\conquest\\RaceSimulator\\drivers.xlsx");
+                int currentSeason = xlWorkbook.Sheets.Count - 1;
+                driverRatingsWorksheet = xlWorkbook.Sheets[1];
+                currentSeasonWorksheet = xlWorkbook.Sheets[currentSeason + 1];
+                driverRatingsRange = driverRatingsWorksheet.UsedRange;
+                currentSeasonRange = currentSeasonWorksheet.UsedRange;
+
+                //Ratings
+                int rowCount = driverRatingsRange.Rows.Count;
+                int colCount = driverRatingsRange.Columns.Count;
+
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    string name = driverRatingsRange.Cells[i, 1].Value2.ToString();
+                    string country = driverRatingsRange.Cells[i, 2].Value2.ToString();
+                    int rating = (int)(driverRatingsRange.Cells[i, 3].Value2);
+                    Drivers.Add(new Driver(name, country, rating));
+                }
+
+                for (int i = 0; i < Drivers.Count; i++)
+                {
+                    DriverRatingsGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                    Grid driverRatingPanel = Drivers[i].DriverRatingPanel();
+                    driverRatingPanel.SetValue(Grid.RowProperty, i);
+                    DriverRatingsGrid.Children.Add(driverRatingPanel);
+                }
+            }
+            finally
+            {
+                //Unload
+                Marshal.ReleaseComObject(driverRatingsRange);
+                Marshal.ReleaseComObject(driverRatingsWorksheet);
+                Marshal.ReleaseComObject(currentSeasonRange);
+                Marshal.ReleaseComObject(currentSeasonWorksheet);
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+            }
+        }
+    }
+}
